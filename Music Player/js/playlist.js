58ed5652a00c64dd70676bdb5a5052485d04e311
Jaishree006 <145030068+Jaishree006@users.noreply.github.com>
2025-06-20 13:31:09 +0530

    Add files via upload

---
+++ Music Player/js/playlist.js
@@ -0,0 +1,26 @@
+let songs = [
+    {
+        name: "6 Shots - NEFFIX",
+        artist: "NEFFEX",
+        img: "img1",
+        audio: "music1"
+    },
+    {
+        name: "No Mercy - TrackTribe",
+        artist: "TrackTribe",
+        img: "img2",
+        audio: "music2"
+    },
+    {
+        name: "Slipping Away - Dyalla",
+        artist: "Dyalla",
+        img: "img3",
+        audio: "music3"
+    },
+    {
+        name: "Desert Planet - Quinacs",
+        artist: "Quinacs",
+        img: "img4",
+        audio: "music4"
+    }
+]
